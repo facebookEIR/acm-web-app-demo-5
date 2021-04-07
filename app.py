38c203575cfc089f4fca7edf254f15f33f921699
Karen Lu <kl.karenlu@gmail.com>
2021-04-07 15:40:19 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+#app.py
+import os
+
+from datetime import date
+from flask import Flask, jsonify, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+# Points SQLAlchemy to Heroku database
+# Replaces postgres:// with postgresql://
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
+# Gets rid of a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate()
+migrate.init_app(app, db)
+
+# IMPORTANT: This must be AFTER creating db variable to prevent
+# circular import issues
+from models import Post
+db.create_all()
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', page='index')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('index.html', page='about')
+
+@app.route('/get_posts', methods=['GET'])
+def get_posts():
+    posts = Post.query.order_by(Post.id.desc())
+    formatted_posts = []
+    for post in posts:
+        formatted_posts.append({
+            "date": post.date.strftime("%m/%d/%y"),
+            "text": post.post_text,
+        })
+    return jsonify({
+        "posts": formatted_posts,
+    })
+
+@app.route('/save_post', methods=['POST'])
+def save_post():
+    today = date.today()
+    post_text = request.form["post-text"]
+    new_post = Post(date=today, post_text=post_text)
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(url_for('index'))
